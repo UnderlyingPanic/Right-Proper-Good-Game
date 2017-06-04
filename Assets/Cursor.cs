49f0a6ebe7991797119b6fe025fda55831a05adf
NATASHA\Stefan <stefanwicks@hotmail.co.uk>
2017-06-04 20:40:47 +0100

    Finalised the Camera, added Click to Move functionality with Enemy and Walkable layers

---
+++ Assets/Cursor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+    CameraRaycaster camRay;
+
+	// Use this for initialization
+	void Start () {
+        camRay = GetComponent<CameraRaycaster>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+}
---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,1 @@
+﻿public enum Layer { Walkable = 8, Enemy = 9, RaycastEndStop = -1 }
