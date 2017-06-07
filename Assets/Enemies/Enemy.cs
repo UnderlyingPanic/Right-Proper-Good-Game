8e2c1a0a60fcac32d9eca966989694bff0899cbf
NATASHA\Stefan <stefanwicks@hotmail.co.uk>
2017-06-07 21:14:14 +0100

    Adding floating Enemy Health Bars

---
+++ Assets/Enemies/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy: MonoBehaviour {
+
+    [SerializeField]
+    float maxHealthPoints=100f;
+    [SerializeField][Range(0,100)]
+    float currentHealthPoints = 100f;
+
+    public float healthAsPercentage
+    {
+        get { return currentHealthPoints / (float)maxHealthPoints; }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
